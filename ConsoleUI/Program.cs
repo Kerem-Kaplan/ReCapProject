939808ee4d19d5351d5eeeb61680ad1bb4a5e626
Kerem-Kaplan <keremkaplan1000@gmail.com>
2022-10-09 15:44:49 +0300

    HomeWork 13

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleUI
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
             //CarTest();
             //CarAdd();
             //CarDelete();
